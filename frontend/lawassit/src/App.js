65346f3ee80454d5a26d700ea36ddebe6f9ddc80
heetsolankiii <solankiheet456@gmail.com>
2026-02-22 19:46:20 +0530

    Modified App.js

--- frontend/lawassit/src/App.js
+++ frontend/lawassit/src/App.js
@@ -1,22 +1,9 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import HowItWorks from "./components/HowItWorks";
-import Categories from "./components/Categories";
-import Footer from "./components/Footer";
-
-import useScrollReveal from "./hooks/useScrollReveal";
+import Home from "./pages/Home";
 
 function App() {
-  useScrollReveal();
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Features />
-      <HowItWorks />
-      <Categories />
-      <Footer />
+      <Home />
     </>
   );
 }
