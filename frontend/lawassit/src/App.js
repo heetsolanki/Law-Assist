0e772bb2e708f22314b5ac9a46e439cefa8c7511
heetsolankiii <solankiheet456@gmail.com>
2026-02-23 21:25:30 +0530

    Added new routes

--- frontend/lawassit/src/App.js
+++ frontend/lawassit/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
